3a46efd9f55076299df66b6aed748c29d6126eef
aman2000raj <amankayshap10@gmail.com>
2024-08-16 14:42:41 +0530

    Add. product page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,8 +8,8 @@ import { SidebarContext } from '../contexts/SidebarContext';
 import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
-  const { isOpen, setIsOpen, handleClose } = useContext(SidebarContext);
-  const { cart, clearCart, total } = useContext(CartContext);
+  const { isOpen, handleClose } = useContext(SidebarContext);
+  const { cart, clearCart, total, itemAmount } = useContext(CartContext);
 
   return (
     <div
@@ -18,7 +18,7 @@ const Sidebar = () => {
       } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
       <div className='flex items-center justify-between py-6 border-b'>
-        <div className='uppercase text-sm font-semibold'>Shopping Bag (0)</div>
+        <div className='uppercase text-sm font-semibold'>{`shopping bag (${itemAmount})`}</div>
         {/* icon */}
         <div
           onClick={handleClose}
