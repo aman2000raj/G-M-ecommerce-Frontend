e5012636084e2d37968b80bcca6626c35e3d9d9b
aman2000raj <amankayshap10@gmail.com>
2024-08-18 23:23:40 +0530

    Refactor. popup

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -101,7 +101,7 @@ const CartProvider = ({ children }) => {
     >
       {children}
       {isSnackbarVisible && (
-        <div className="fixed bottom-4 left-6 bg-gray-800 text-white px-4 py-2 rounded shadow-lg transition-opacity duration-300">
+        <div className="fixed bottom-4 left-6 bg-gray-800 text-white px-4 py-2 rounded shadow-lg transition-opacity duration-300 border-2 border-l-pink-50">
           Added to Cart
         </div>
       )}
