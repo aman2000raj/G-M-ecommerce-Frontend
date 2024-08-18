bd681cc4b136d0dd24208ebe101de015735ab01a
aman2000raj <amankayshap10@gmail.com>
2024-08-18 23:18:04 +0530

    Add. logout and username to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { SidebarContext } from '../contexts/SidebarContext';
 import { CartContext } from '../contexts/CartContext';
+import { UserContext } from '../contexts/UserContext';
 import { BsBag, BsPerson, BsSearch } from 'react-icons/bs';
 import { Link, useLocation } from 'react-router-dom';
 import Logo from '../img/logo.svg';
@@ -9,15 +10,19 @@ const Header = () => {
   const [isActive, setIsActive] = useState(false);
   const { isOpen, setIsOpen } = useContext(SidebarContext);
   const { itemAmount } = useContext(CartContext);
+  const { user, logout } = useContext(UserContext);
   const location = useLocation();
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
+    const handleScroll = () => {
       window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
-    });
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  const isHomePage = location.pathname !== '/login' && location.pathname !== '/signup';
+  const isHomePage =
+    location.pathname !== '/login' && location.pathname !== '/signup';
 
   return (
     <header
@@ -27,22 +32,32 @@ const Header = () => {
     >
       <div className='container mx-auto max-w-screen-xl flex items-center justify-between h-full px-6'>
         <Link to={'/'}>
-          <div className='flex flex-row items-center justify-center'>
-            G&M STORE
-            <img className='w-[40px] ml-2' src={Logo} alt='' />
+          <div className='flex flex-row items-center justify-center space-x-2 md:space-x-4'>
+            <div className='text-xl md:text-2xl font-bold'>
+              G&M STORE
+            </div>
+            <img className='w-[40px]' src={Logo} alt='Logo' />
           </div>
         </Link>
         {isHomePage && (
-          <div className='cursor-pointer flex relative'>
+          <div className='cursor-pointer flex items-center relative space-x-4 ml-4 md:ml-6'>
             {/* Search Icon */}
-            <BsSearch className='text-2xl cursor-pointer mx-4' />
-            {/* Login Icon */}
-            <Link to={'/login'}>
-              <BsPerson className='text-2xl cursor-pointer mx-6' />
-            </Link>
-            <div onClick={() => setIsOpen(!isOpen)}>
-              <BsBag className='text-2xl' />
-              <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center'>
+            <BsSearch className='text-2xl md:text-3xl cursor-pointer mx-2 md:mx-4' />
+            {user ? (
+              <div className='text-xl md:text-2xl flex items-center space-x-4'>
+                <span>{user.firstName}</span>
+                <button onClick={logout} className='text-sm md:text-base'>
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <Link to={'/login'}>
+                <BsPerson className='text-2xl md:text-3xl cursor-pointer mx-2 md:mx-4' />
+              </Link>
+            )}
+            <div onClick={() => setIsOpen(!isOpen)} className='relative'>
+              <BsBag className='text-2xl md:text-3xl' />
+              <div className='bg-red-500 absolute -right-2 -bottom-2 text-[10px] md:text-[12px] w-[16px] md:w-[18px] h-[16px] md:h-[18px] text-white rounded-full flex justify-center items-center'>
                 {itemAmount}
               </div>
             </div>
