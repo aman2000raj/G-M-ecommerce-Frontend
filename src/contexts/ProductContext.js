2bba6aa1f36bcfe42f163ee0b08eac3b8334311e
aman2000raj <amankayshap10@gmail.com>
2024-08-18 23:25:44 +0530

    Add. connect to backend.

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -7,10 +7,9 @@ const ProductProvider = ({ children }) => {
 
   useEffect(() => {
     const fetchedProducts = async () => {
-      const response = await fetch('https://fakestoreapi.com/products');
+      const response = await fetch('http://localhost:8080/api/products');
       const data = await response.json();
       setProducts(data);
-      // console.log(data);
     };
 
     fetchedProducts();
