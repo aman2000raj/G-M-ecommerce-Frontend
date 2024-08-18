39033a9c94a8e6f082931b459a50fd8b737095db
aman2000raj <amankayshap10@gmail.com>
2024-08-18 23:26:26 +0530

    Add. User Provider.

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import App from './App';
 import ProductProvider from './contexts/ProductContext';
 import SidebarProvider from './contexts/SidebarContext';
 import CartProvider from './contexts/CartContext';
+import UserProvider from './contexts/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <SidebarProvider>
-    <CartProvider>
-    <ProductProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ProductProvider>
-    </CartProvider>
-  </SidebarProvider>
+  <UserProvider>
+    <SidebarProvider>
+      <CartProvider>
+        <ProductProvider>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </ProductProvider>
+      </CartProvider>
+    </SidebarProvider>
+  </UserProvider>
 );
